5bd360b237773ffb9db490532b341f73dd3bc76c
Sudhakar J <35969068+Sudhakar7J@users.noreply.github.com>
2023-07-01 12:15:29 +0530

    aws

--- config/database.js
+++ config/database.js
@@ -23,6 +23,15 @@ module.exports = ({ env }) => {
             true
           ),
         },
+        client: "mysql",
+        connection: {
+          host: env("DATABASE_HOST", "127.0.0.1"),
+          port: env.int("DATABASE_PORT", 5432),
+          database: env("DATABASE_NAME", "strapi"),
+          user: env("DATABASE_USERNAME", ""),
+          password: env("DATABASE_PASSWORD", ""),
+        },
+        useNullAsDefault: true,
       },
       pool: {
         min: env.int("DATABASE_POOL_MIN", 2),
--- config/plugins.js
+++ config/plugins.js
@@ -14,5 +14,22 @@ module.exports = ({ env }) => ({
         },
       },
     },
+    provider: "aws-s3",
+    providerOptions: {
+      accessKeyId: env("AWS_ACCESS_KEY_ID"),
+      secretAccessKey: env("AWS_ACCESS_SECRET"),
+      region: env("AWS_REGION"),
+      params: {
+        Bucket: env("AWS_BUCKET_NAME"),
+      },
+    },
+    actionOptions: {
+      upload: {
+        ACL: null,
+      },
+      uploadStream: {
+        ACL: null,
+      },
+    },
   },
 });
